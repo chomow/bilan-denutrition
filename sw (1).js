34cd1f16ec0231ae5d32a52055cf6001a786c6e7
chomow <dlaplagne16@gmail.com>
2025-11-30 21:11:26 +0100

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,71 @@
+const CACHE_NAME = 'denutrition-has-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://unpkg.com/lucide@latest'
+];
+
+// Installation du Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache ouvert');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activation du Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Suppression ancien cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interception des requêtes
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Retourne depuis le cache si disponible
+        if (response) {
+          return response;
+        }
+        
+        // Sinon, fait une requête réseau
+        return fetch(event.request).then(response => {
+          // Vérifie si la réponse est valide
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          // Clone la réponse
+          const responseToCache = response.clone();
+
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        });
+      })
+      .catch(() => {
+        // En cas d'erreur, retourne la page d'accueil en cache
+        return caches.match('./index.html');
+      })
+  );
+});
